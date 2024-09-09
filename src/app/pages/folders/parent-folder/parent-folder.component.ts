6a84081e31817d48d5d2a2ec3a66fa6084978398
Sanjai <sanjai@wildrepublictech.com>
2024-09-09 17:26:01 +0530

    Syle updated

---
+++ src/app/pages/folders/parent-folder/parent-folder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-folder',
+  standalone: true,
+  imports: [],
+  templateUrl: './parent-folder.component.html',
+  styleUrl: './parent-folder.component.css'
+})
+export class ParentFolderComponent {
+
+}
